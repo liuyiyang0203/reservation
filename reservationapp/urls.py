fffc80f22eaea383e670b37ec8507ecce6ee3a5b
liuyiyang0203 <2447473760@qq.com>
2019-06-17 09:37:04 +0800

    no

--- reservationapp/urls.py
+++ reservationapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index),
-    path('index/index1', views.index1),
-    path('index/index2', views.index2),
+    path('index/', views.index, name='index'),
+    path('index1/', views.index1, name='index1'),
+    path('index2/', views.index2, name='index2'),
 ]

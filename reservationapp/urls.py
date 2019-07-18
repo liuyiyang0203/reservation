0ed2b43b7a5d77c25d3328d171742d8fc80b7334
liuyiyang0203 <2447473760@qq.com>
2019-07-18 10:05:33 +0800

    no

--- reservationapp/urls.py
+++ reservationapp/urls.py
@@ -3,8 +3,13 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('index_wx/', views.index_wx, name='index_wx'),
     path('index1/', views.index1, name='index1'),
+    path('index1_wx/', views.index1_wx, name='index1_wx'),
     path('index2/', views.index2, name='index2'),
+    path('index2_wx/', views.index2_wx, name='index2_wx'),
     path('index3/', views.index3, name='index3'),
+    path('index3_wx/', views.index3_wx, name='index3_wx'),
     path('user_logout/', views.user_logout, name='user_logout'),
+    path('user_logout_wx/', views.user_logout_wx, name='user_logout_wx'),
 ]
--- reservationapp/views.py
+++ reservationapp/views.py
@@ -23,6 +23,24 @@ def index(request):
             return render(request, 'reservationapp/index.html',{'error_msg': error_msg})
     return render(request, 'reservationapp/index.html')
 
+
+def index_wx(request):
+    error_msg = ''
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        print(username)
+        print(password)
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return HttpResponseRedirect('/index3_wx')
+        else:
+            error_msg = '用户名密码错误'
+            return render(request, 'reservationapp/index_wx.html',{'error_msg': error_msg})
+    return render(request, 'reservationapp/index_wx.html')
+
+
 @login_required
 def index1(request):
     place_name = Place.objects.all()
@@ -54,6 +72,39 @@ def index1(request):
         return render(request, 'reservationapp/index1.html', {'error_msg': error_msg, 'place_name': place_name})
     return render(request, 'reservationapp/index1.html', {'place_name': place_name})
 
+
+@login_required
+def index1_wx(request):
+    place_name = Place.objects.all()
+    error_msg = ''
+    if request.method == 'POST':
+        place = request.POST.get('place', None)
+        people = request.POST.get('people', None)
+        date = request.POST.get('date', None)
+        time = request.POST.get('time', None)
+        number = request.POST.get('number', None)
+        remark = request.POST.get('remark', None)
+        if place == '':
+            error_msg = '请正确填写预约场地'
+        elif people == '':
+            error_msg = '请正确填写预约人员'
+        elif date == '' or time == '':
+            error_msg = '请正确填写预约时间'
+        elif number == '':
+            error_msg = '请正确填写联系方式'
+        else:
+            a = Notice.objects.filter(place=place, date=date, time=time)
+            if a.count() == 0:
+                b = Notice(place=place, people=people, date=date, time=time, number=number, remark=remark)
+                b.save()
+                error_msg = '预约成功'
+                # return render(request, 'reservationapp/index1.html', {'error_msg': error_msg})
+            else:
+                error_msg = '预约失败，该场地在此时段已经被预约'
+        return render(request, 'reservationapp/index1_wx.html', {'error_msg': error_msg, 'place_name': place_name})
+    return render(request, 'reservationapp/index1_wx.html', {'place_name': place_name})
+
+
 @login_required
 def index2(request):
     instrument_name = Instrument.objects.all()
@@ -66,7 +117,7 @@ def index2(request):
         number = request.POST.get('number', None)
         remark = request.POST.get('remark', None)
         if place == '':
-            error_msg = '请正确填写预约场地'
+            error_msg = '请正确填写预约仪器'
         elif people == '':
             error_msg = '请正确填写预约人员'
         elif date == '' or time == '':
@@ -85,6 +136,39 @@ def index2(request):
         return render(request, 'reservationapp/index2.html', {'error_msg': error_msg, 'instrument': instrument_name})
     return render(request, 'reservationapp/index2.html', {'instrument': instrument_name})
 
+
+@login_required
+def index2_wx(request):
+    instrument_name = Instrument.objects.all()
+    error_msg = ''
+    if request.method == 'POST':
+        place = request.POST.get('place', None)
+        people = request.POST.get('people', None)
+        date = request.POST.get('date', None)
+        time = request.POST.get('time', None)
+        number = request.POST.get('number', None)
+        remark = request.POST.get('remark', None)
+        if place == '':
+            error_msg = '请正确填写预约仪器'
+        elif people == '':
+            error_msg = '请正确填写预约人员'
+        elif date == '' or time == '':
+            error_msg = '请正确填写预约时间'
+        elif number == '':
+            error_msg = '请正确填写联系方式'
+        else:
+            b = Notice.objects.filter(place=place, date=date, time=time)
+            if b.count() == 0:
+                c = Notice(place=place, people=people, date=date, time=time, number=number, remark=remark)
+                c.save()
+                error_msg = '预约成功'
+                # return render(request, 'reservationapp/index1.html', {'error_msg': error_msg})
+            else:
+                error_msg = '预约失败，该仪器在此时段已经被预约'
+        return render(request, 'reservationapp/index2_wx.html', {'error_msg': error_msg, 'instrument': instrument_name})
+    return render(request, 'reservationapp/index2_wx.html', {'instrument': instrument_name})
+
+
 @login_required
 def index3(request):
     e = Notice.objects.all().order_by('-date', 'time')
@@ -96,6 +180,19 @@ def index3(request):
         e = paginator.page(1)
     return render(request, 'reservationapp/index3.html', {'e': e})
 
+
+@login_required
+def index3_wx(request):
+    e = Notice.objects.all().order_by('-date', 'time')
+    paginator = Paginator(e, 10)
+    page = request.GET.get('page')
+    try:  # 获取某页
+        e = paginator.page(page)
+    except :  # 如果 page 参数不为正整数，显示第一页
+        e = paginator.page(1)
+    return render(request, 'reservationapp/index3_wx.html', {'e': e})
+
+
 @csrf_exempt
 def user_logout(request):
     if request.user.is_authenticated:
@@ -105,3 +202,10 @@ def user_logout(request):
         return HttpResponseRedirect('/index')
 
 
+@csrf_exempt
+def user_logout_wx(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return HttpResponseRedirect('/index_wx')
+    else:
+        return HttpResponseRedirect('/index_wx')
